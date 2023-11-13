14f1346daa4cdaad8032abcb51e9402218fbc880
Jess Roque <68854057+jessdolt@users.noreply.github.com>
2023-11-13 17:38:45 +0800

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { UserButton } from "@clerk/nextjs"
+import React from "react"
+
+const DashboardPage = () => {
+  return <div>Dashboard Page (protected)</div>
+}
+
+export default DashboardPage
